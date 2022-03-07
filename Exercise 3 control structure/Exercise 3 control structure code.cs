28809f4da0dff99a0101e484124bebb0c960306a
EspelimbergoJP-ITE-121 <100054879+EspelimbergoJP@users.noreply.github.com>
2022-03-07 17:14:07 +0800

    Create Exercise 3 control structure code.cs

---
+++ Exercise 3 control structure/Exercise 3 control structure code.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Io;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CSharp_Shell
+{
+    
+      public class Program
+      {
+            public static void Main()
+            {
+                  Console.WriteLine("Example code")
+             }
+       }
+}
